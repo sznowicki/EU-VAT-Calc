d7e69e2b9ce4ecd8f60b0e4ec8138f0d6e01b33e
Szymon Nowicki <sznowicki@eskalacja.com>
2023-03-11 17:46:21 +0100

    Fix tests

--- integration/cjs/index.js
+++ integration/cjs/index.js
@@ -7,7 +7,7 @@ const main = () => {
     });
 
     const vat = calc.getVat('DE', false);
-
+    console.log(vat.standard_rate);
     if (typeof vat.standard_rate !== 'number') {
         throw new Error('Invalid return value');
     }
